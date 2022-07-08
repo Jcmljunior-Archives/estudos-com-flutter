42f7d349331ea64c0224e45420cc7f2ed1c3acf4
Julio Cesar <jcmljunior@gmail.com>
2022-07-08 20:56:43 -0300

    pequenas correções

--- my_first_application/lib/view/initial.view.dart
+++ my_first_application/lib/view/initial.view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../controller/initial.dart';
+import '../controller/initial.controller.dart';
 
 class InitialView extends StatelessWidget {
   /// Construtor.
